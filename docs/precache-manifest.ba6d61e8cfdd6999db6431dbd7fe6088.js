f3df8dd6aae5fdfff8cb079f6f0adc4fb40906ac
Patricia Ramos <patriciaramosf94@gmail.com>
2020-05-16 17:36:41 +0200

    changin index for github page responsive

---
+++ docs/precache-manifest.ba6d61e8cfdd6999db6431dbd7fe6088.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91cb45b4bf986e3ea80caedb4c7c3109",
+    "url": "./index.html"
+  },
+  {
+    "revision": "aed0b8312abe0ce68589",
+    "url": "./static/css/main.32df4a0c.chunk.css"
+  },
+  {
+    "revision": "27539a71dde17e9394a9",
+    "url": "./static/js/2.1f957dfa.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.1f957dfa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aed0b8312abe0ce68589",
+    "url": "./static/js/main.bcd1c848.chunk.js"
+  },
+  {
+    "revision": "a062e1f0e1f924f559d3",
+    "url": "./static/js/runtime-main.2afa580b.js"
+  },
+  {
+    "revision": "d5753ec1285dbd57042e9c8ceaeb4c98",
+    "url": "./static/media/mygiphy.d5753ec1.png"
+  },
+  {
+    "revision": "4eb119593ce507f8ef0f11394949f415",
+    "url": "./static/media/pantera.4eb11959.png"
+  },
+  {
+    "revision": "7aeb0ce4445e25ff6f0123ac97aec7be",
+    "url": "./static/media/patricartoon.7aeb0ce4.jpeg"
+  },
+  {
+    "revision": "c8254fbae88437cfb7baa1524c8dfd9e",
+    "url": "./static/media/pixar.c8254fba.png"
+  },
+  {
+    "revision": "099592e953c6cf990276cba84b13d7b5",
+    "url": "./static/media/responsive.099592e9.png"
+  },
+  {
+    "revision": "9e3db3efb07adab9f82b32e8bb03201a",
+    "url": "./static/media/sticker.9e3db3ef.png"
+  }
+]);
