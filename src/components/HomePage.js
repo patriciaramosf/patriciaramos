41a427cbbbfa41e1314785ff3d47c5ed5cdeeddc
Patricia Ramos <patriciaramosf94@gmail.com>
2020-05-17 12:22:38 +0200

    final responsive mobile for github

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import photo from '../images/cartoon.png'
+import photo from '../images/cartoonbn.png'
 import ProjectPage from './ProjectPage.js'
 const HomePage =(props)=>{
     return(
