112f3358707e8d3e4801f3d38a1927853fb6e7b6
Patricia Ramos <patriciaramosf94@gmail.com>
2020-05-18 12:20:44 +0200

    final web css

---
+++ docs/precache-manifest.0b230f77c31bb9ecf2313da27cd2b7c0.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "828c89da1cd3fd0e73e2723c8360ddce",
+    "url": "./index.html"
+  },
+  {
+    "revision": "420df15d20b020c69961",
+    "url": "./static/css/main.ec172586.chunk.css"
+  },
+  {
+    "revision": "27539a71dde17e9394a9",
+    "url": "./static/js/2.1f957dfa.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.1f957dfa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "420df15d20b020c69961",
+    "url": "./static/js/main.439f1d42.chunk.js"
+  },
+  {
+    "revision": "a062e1f0e1f924f559d3",
+    "url": "./static/js/runtime-main.2afa580b.js"
+  },
+  {
+    "revision": "d5753ec1285dbd57042e9c8ceaeb4c98",
+    "url": "./static/media/mygiphy.d5753ec1.png"
+  },
+  {
+    "revision": "4eb119593ce507f8ef0f11394949f415",
+    "url": "./static/media/pantera.4eb11959.png"
+  },
+  {
+    "revision": "c8254fbae88437cfb7baa1524c8dfd9e",
+    "url": "./static/media/pixar.c8254fba.png"
+  },
+  {
+    "revision": "00531a019c832fedc4e5125a37d394ef",
+    "url": "./static/media/recortada.00531a01.png"
+  },
+  {
+    "revision": "099592e953c6cf990276cba84b13d7b5",
+    "url": "./static/media/responsive.099592e9.png"
+  },
+  {
+    "revision": "9e3db3efb07adab9f82b32e8bb03201a",
+    "url": "./static/media/sticker.9e3db3ef.png"
+  }
+]);
