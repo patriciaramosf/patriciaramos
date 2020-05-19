af5383a661d31ceadf8a19440d8149bc68529382
Patricia Ramos <patriciaramosf94@gmail.com>
2020-05-19 14:42:24 +0200

    title fixed

--- src/texts/home.js
+++ src/texts/home.js
@@ -1,6 +1,6 @@
 const HOME = {
     es : {
-        title:'Hola',
+        title:'¡Hola!',
         introduction:'Tras varias experiencias en el sector del Marketing Digital, decidí formarme en Adalab como desarrolladora frontend.',
         description:'Soy de Madrid, me considero una persona alegre, curiosa y colaborativa, y estoy buscando un trabajo en el que poder aportar y aprender a diario.',
         hobbies :{
@@ -13,7 +13,7 @@ const HOME = {
 
     },
     en : {
-        title:'Hello',
+        title:'Hello!',
         introduction:'After several experiences in Digital Marketing, I decided to learn coding at Adalab.',
         description:'I´m from Madrid, I consider myself a happy, curious and collaborative person, and I´m looking for a job where I can put all my skills and learn everyday.',
         hobbies :{
