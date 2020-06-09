d9c42c0bd4f3fc2b27df6461c9f92daadfb0d0ad
Patricia Ramos <patriciaramosf94@gmail.com>
2020-06-09 16:49:42 +0200

    adding title

---
+++ docs/precache-manifest.163f020db45b899c788da722511e33f2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa0e0e96adf92f8ff66c22c552a0a737",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ba4b4406101c71ee1bb5",
+    "url": "./static/css/main.40d57209.chunk.css"
+  },
+  {
+    "revision": "6e7d79ac76008ef437c0",
+    "url": "./static/js/2.2ae7f27e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.2ae7f27e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba4b4406101c71ee1bb5",
+    "url": "./static/js/main.bfc41a71.chunk.js"
+  },
+  {
+    "revision": "a062e1f0e1f924f559d3",
+    "url": "./static/js/runtime-main.2afa580b.js"
+  }
+]);
