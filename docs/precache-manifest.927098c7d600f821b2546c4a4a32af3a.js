a7301d9a4a0577ab5265aa97ee2d9c3a9334b400
Patricia Ramos <patriciaramosf94@gmail.com>
2020-06-15 19:24:06 +0200

    background bug solved

---
+++ docs/precache-manifest.927098c7d600f821b2546c4a4a32af3a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74758ec88954956f55974ea2036a9566",
+    "url": "./index.html"
+  },
+  {
+    "revision": "6e77c22885df4b315d55",
+    "url": "./static/css/main.784cf9af.chunk.css"
+  },
+  {
+    "revision": "6e7d79ac76008ef437c0",
+    "url": "./static/js/2.2ae7f27e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.2ae7f27e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6e77c22885df4b315d55",
+    "url": "./static/js/main.40837053.chunk.js"
+  },
+  {
+    "revision": "a062e1f0e1f924f559d3",
+    "url": "./static/js/runtime-main.2afa580b.js"
+  }
+]);
