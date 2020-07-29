c7d18dc125058497051751ef90533665b0b1b1dd
Patricia Ramos <patriciaramosf94@gmail.com>
2020-07-29 22:18:47 +0100

    adding project

---
+++ docs/precache-manifest.2f309b7156ce0b897a9c6d281753fc81.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8eb86af5901d14416fa0a0132ffa8298",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e830abe0ce1c9afee716",
+    "url": "./static/css/main.11467dfe.chunk.css"
+  },
+  {
+    "revision": "6e7d79ac76008ef437c0",
+    "url": "./static/js/2.2ae7f27e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.2ae7f27e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e830abe0ce1c9afee716",
+    "url": "./static/js/main.bd53436d.chunk.js"
+  },
+  {
+    "revision": "a062e1f0e1f924f559d3",
+    "url": "./static/js/runtime-main.2afa580b.js"
+  }
+]);
