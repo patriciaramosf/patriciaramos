c6a877ba846e8a1d2347c4251f84b27d99829723
Patricia Ramos <patriciaramosf94@gmail.com>
2021-11-09 18:45:59 +0000

    new text

--- src/texts/home.js
+++ src/texts/home.js
@@ -1,8 +1,9 @@
 const HOME = {
     es : {
         title:'¡Hola!',
-        introduction:'Hace más de un año que decidí dejar el marketing digital para formarme como desarrolladora frontend, y a dia de hoy... ¡no podría estar más orgullosa de la decisión que tomé! Me encanta el código, lo rápido que hace que pase el tiempo, y lo mucho que me permite explotar tanto la lógica como la creatividad.',
-        description:'Actualmente trabajo como Frontend (Vue), pero también tengo experiencia en React, (este portfolio es prueba de ello 😆), y React Native, con el cual estoy empezando a hacer mis primeros pinitos en el mundo del desarrollo movil.',
+        introduction:'Me encanta el código, lo rápido que hace que pase el tiempo, y lo mucho que me permite explotar tanto la lógica como la creatividad.',
+        description:'Actualmente trabajo como Frontend (Vue), pero también tengo experiencia en React, (este portfolio es prueba de ello 😆).',
+        /* y Python y Django, con el cual estoy empezando a hacer mis primeros pinitos en el mundo backend. */
         description2:'Soy de Madrid, y me considero una persona alegre, curiosa y colaborativa',
         hobbies :{
             title:'Me encanta',
@@ -15,8 +16,9 @@ const HOME = {
     },
     en : {
         title:'Hello!',
-        introduction:'It´s been over a year since I decided to change from digital marketing to frontend development... and I couldn´t be more proud of the decision I made! I´m passionate about coding, how it makes time fly and how much it allows me to use both logic and creativity.',
-        description:'Currently, I´m working as a Frontend (Vue), but I also have experience in React (this portfolio is a proof of this 😆), and React Native, with which I am starting to take my first steps in the mobile development world.',
+        introduction:'I´m passionate about coding, how it makes time fly and how much it allows me to use both logic and creativity.',
+        description:'Currently, I´m working as a Frontend (Vue), but I also have experience in React (this portfolio is a proof of this 😆).',
+        /* and Python, with which I am starting to take my first steps in the backend development world. */
         description2:'I´m from Madrid, and I consider myself a happy, curious and collaborative person.',
         hobbies :{
             title:'I love',
